276b4fb24a10f618597b1f60bd484520a5b5404a
kingsofcoding <simplecooldude1@gmail.com>
2016-07-30 00:59:35 +0000

    check for valid URL with .value

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,8 +4,8 @@ function check(){
     var trimmedTextAreaValue=inputValue.trim(); 
     if(trimmedTextAreaValue!=="") {
         if (inputValue.split("").slice(0, 6).join("") !== "http://" || inputValue.split("").slice(0, 7).join("") !== "https://") {
-            inputValue.value = "http://" + inputValue.value;
-            console.log(inputValue);
+            inputValue = "http://" + inputValue;
+            document.forms["form"].submit();
         } else {
             document.forms["form"].submit();
         }
