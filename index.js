d79f6c9e7d6da4ad262481a6c0d7347acfb1e88e
kingsofcoding <simplecooldude1@gmail.com>
2016-07-30 01:08:32 +0000

    do things on getting :id

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var util = require('./src/util.js');
 var storage = require('./models/storage.js');
+var view = require('./src/view.js');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/assets'));
@@ -16,6 +17,21 @@ app.post('/new', function(req, res) {
     storage.addURL(url);
 });
 
+app.get('/:id', function(req, res) {
+   var id = req.params.id;
+   res.set('Content-Type', 'text/html');
+   storage.getURL(id).then(function(url) {
+      if(!url) {
+          res.end('404 Not Found');
+          util.log("[SNIP] 404 Not Found", "yellow");
+      } else {
+          var html = view.render(url.id);
+          res.send(html);
+          util.log("User visited /" + id, "green");
+      }
+   });
+});
+
 app.listen(process.env.PORT, function (req, res) {
-    util.log("Listening", "green");
+    util.log("[SNIP] Listening", "green");
 });
