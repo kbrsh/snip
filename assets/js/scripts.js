eb908400c84649e8897f3a538f1622bf3acd5554
kingsofcoding <simplecooldude1@gmail.com>
2016-07-30 00:54:18 +0000

    check for valid URL

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,10 +2,13 @@ function check(){
     String.prototype.trim = function() { return this.replace(/^\s+|\s+$/g, ""); }; 
     var inputValue=document.getElementById('url').value; 
     var trimmedTextAreaValue=inputValue.trim(); 
-    if(trimmedTextAreaValue!="") { 
+    if(trimmedTextAreaValue!=="") {
+        if (inputValue.split("").slice(0, 6).join("") !== "http://" || inputValue.split("").slice(0, 7).join("") !== "https://") {
+            inputValue = "http://" + inputValue;
+        }
         document.forms["form"].submit();
     } else {
-        alert('You need to enter a URL!');
+        alert('You need to enter a valid URL!');
         return false;
     }
 } 
