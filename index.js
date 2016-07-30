2e72b030745e8eea4b31d10703632813163a35dd
kingsofcoding <simplecooldude1@gmail.com>
2016-07-30 01:03:11 +0000

    storage variable

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
 var util = require('./src/util.js');
+var storage = require('./models/storage.js');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/assets'));
