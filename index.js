fcf3d6fbef3e78f405d6b6b4d283e78122618c72
kingsofcoding <simplecooldude1@gmail.com>
2016-07-30 00:00:59 +0000

    require util

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
+var util = require('./src/util.js');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/assets'));
