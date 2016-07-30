d7b1cf8f79fceb5d50a96dfb564bb64a1272e9ae
kingsofcoding <simplecooldude1@gmail.com>
2016-07-30 06:03:58 +0000

    add http if no http already

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,14 +1,17 @@
+function addhttp(url) {
+    if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
+        url = "http://" + url;
+    }
+    return url;
+}
+
 function check(){ 
     String.prototype.trim = function() { return this.replace(/^\s+|\s+$/g, ""); }; 
     var inputValue=document.getElementById('url').value; 
     var trimmedTextAreaValue=inputValue.trim(); 
     if(trimmedTextAreaValue!=="") {
-        if (inputValue.split("").slice(0, 6).join("") !== "http://" || inputValue.split("").slice(0, 7).join("") !== "https://") {
-            inputValue = "http://" + inputValue;
-            document.forms["form"].submit();
-        } else {
-            document.forms["form"].submit();
-        }
+        document.getElementById('url').value = addhttp(inputValue);
+        document.forms["form"].submit();
     } else {
         alert('You need to enter a valid URL!');
         return false;
