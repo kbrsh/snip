f5da2732f9d958dd47bc64eb21f0aeaa92c98484
kingsofcoding <simplecooldude1@gmail.com>
2016-08-08 23:19:34 +0000

    comment out 404 logs (they are inaccurate)

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.get('/:id/api', function(req, res) {
    storage.getURL(id).then(function(url) {
       if(!url) {
           res.end('404 Not Found');
-          util.log("[SNIP] 404 Not Found", "yellow");
+        //   util.log("[SNIP] 404 Not Found", "yellow");
       } else {
           res.header('Content-Type', 'application/json');
           res.header("Access-Control-Allow-Origin", "*");
@@ -60,7 +60,7 @@ app.get('/:id/api', function(req, res) {
    storage.getURL(id).then(function(url) {
       if(!url) {
           res.end('404 Not Found');
-          util.log("[SNIP] 404 Not Found", "yellow");
+        //   util.log("[SNIP] 404 Not Found", "yellow");
       } else {
           res.header('Content-Type', 'text/html');
           res.send();
