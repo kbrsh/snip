1e6d1f5edb94fd1885b359a7e0184efac775f6c1
kingsofcoding <simplecooldude1@gmail.com>
2016-08-12 17:41:03 +0000

    404 status

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get('/:id', function(req, res) {
    var id = req.params.id;
    storage.getURL(id).then(function(url) {
       if(!url) {
-          res.end('404 Not Found');
+          res.status(404).end('404 Not Found');
           //util.log("[SNIP] 404 Not Found", "yellow");
       } else {
           url.visits = url.visits + 1
