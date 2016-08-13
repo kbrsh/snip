8c7c46e82489a8b161954685e4886f2fb7417ac9
kingsofcoding <simplecooldude1@gmail.com>
2016-08-13 16:26:17 +0000

    grammar adapts to number of visits, fixes #1

--- assets/js/stats/stats.js
+++ assets/js/stats/stats.js
@@ -1,4 +1,4 @@
 if(document.getElementById("visits").innerHTML === "1") {
-    document.getElementById("p").innerHTML = "person"
-    document.getElementById("h").innerHTML = "has"
+    document.getElementById("p").innerHTML = "person";
+    document.getElementById("h").innerHTML = "has";
 }
