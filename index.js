ce81f1297a563237ff73ffc868d9a2d7dfc9afe1
kingsofcoding <simplecooldude1@gmail.com>
2016-08-16 04:24:58 +0000

    delete comment

--- index.js
+++ index.js
@@ -43,7 +43,6 @@ app.get('/:id/api', function(req, res) {
    storage.getURL(id).then(function(url) {
       if(!url) {
           res.status(404).end('404 Not Found');
-        //   util.log("[SNIP] 404 Not Found", "yellow");
       } else {
           res.header('Content-Type', 'application/json');
           res.header("Access-Control-Allow-Origin", "*");
