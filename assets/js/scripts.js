1353a7d84a69cc26fdf40f74a53352cb31565b1c
Kabir Shah <simplecooldude1@gmail.com>
2016-08-21 15:05:27 -0700

    fix up regexp

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,14 +2,15 @@ function addhttp(url) {
     if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
         url = "http://" + url;
     }
+    if(url.split("")[url.length-1] !== "/") {
+      url = url + "/"
+    }
     return url;
 }
 
 function ValidURL(str) {
-  var pattern = new RegExp("(http|ftp|https)://[\w-]+(\.[\w-]*)+([\w.,@?^=%&amp;:/~+#-]*[\w@?^=%&amp;/~+#-])?");
+  var pattern = new RegExp(/([https|http]:\/\/)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]\/*)/);
   if(!pattern.test(str)) {
-    document.body.style.backgroundColor = "#D7263D"
-    document.body.style.color = "#F5F5F5"
     return false;
   } else {
     return true;
@@ -18,13 +19,12 @@ function ValidURL(str) {
 
 function check(){
     var inputValue=document.getElementById('url').value;
-
-    if(ValidURL(inputValue)) {
+    if(ValidURL(addhttp(inputValue))) {
         document.getElementById('url').value = addhttp(inputValue);
         document.forms["form"].submit();
     } else {
-      document.body.style.backgroundColor = "#D7263D"
-      document.body.style.color = "#F5F5F5"
-      return false;
+        document.body.style.backgroundColor = "#D7263D"
+        document.body.style.color = "#F5F5F5"
+        return false;
     }
 }
