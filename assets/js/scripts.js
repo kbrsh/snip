4a38b48c1d6fdce60dba6605b3b44852f6957def
Gopal Shah <simplecooldude1@gmail.com>
2016-08-21 14:04:05 -0700

    validate url function

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,11 +5,26 @@ function addhttp(url) {
     return url;
 }
 
-function check(){ 
-    String.prototype.trim = function() { return this.replace(/^\s+|\s+$/g, ""); }; 
-    var inputValue=document.getElementById('url').value; 
-    var trimmedTextAreaValue=inputValue.trim(); 
-    if(trimmedTextAreaValue!=="") {
+function ValidURL(str) {
+  var pattern = new RegExp('^(https?:\/\/)?'+ // protocol
+    '((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|'+ // domain name
+    '((\d{1,3}\.){3}\d{1,3}))'+ // OR ip (v4) address
+    '(\:\d+)?(\/[-a-z\d%_.~+]*)*'+ // port and path
+    '(\?[;&a-z\d%_.~+=-]*)?'+ // query string
+    '(\#[-a-z\d_]*)?$','i'); // fragment locater
+  if(!pattern.test(str)) {
+    alert("Please enter a valid URL.");
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function check(){
+    String.prototype.trim = function() { return this.replace(/^\s+|\s+$/g, ""); };
+    var inputValue=document.getElementById('url').value;
+    var trimmedTextAreaValue=inputValue.trim();
+    if(trimmedTextAreaValue!=="" || ) {
         document.getElementById('url').value = addhttp(inputValue);
         document.forms["form"].submit();
     } else {
@@ -17,4 +32,4 @@ function check(){
         document.body.style.color = "#F5F5F5"
         return false;
     }
-} 
+}
