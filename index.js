3fe77d18ec88a677c12fe6712b3cd1fe37551020
Kabir Shah <simplecooldude1@gmail.com>
2016-09-10 16:26:42 -0700

    comment

--- index.js
+++ index.js
@@ -51,14 +51,24 @@ app.post('/new', function(req, res) {
     util.log("[SNIP] User posted to /new", "green");
 });
 
+
+// GET '/:id'
 app.get('/:id', function(req, res) {
+  //  Get ID from URL
    var id = req.params.id;
+
+  //  Get URL from storage
    storage.getURL(id).then(function(url) {
       if(!url) {
+        // If it doesn't exist, send 404
           util.showNotFound(res);
       } else {
+          // If found
+          // Update stats
           url.increment('visits', {by: 1});
+          // Redirect to URL
           res.redirect(url.url);
+          //  Log it down
           util.log("[SNIP] User redirected from /" + id, "green");
       }
    });
