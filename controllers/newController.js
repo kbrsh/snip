dbf7b91a3c3ceff08f0a99669949badbb14991c9
Gopal Shah <simplecooldude1@gmail.com>
2016-09-10 16:18:41 -0700

    update newcontroller to use new template

--- controllers/newController.js
+++ controllers/newController.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-var template = fs.readFileSync("./views/template/template.html", "utf-8");
+var template = fs.readFileSync("./views/newTemplate/new.html", "utf-8");
 
 var render = function(url) {
     return template.replace(/{{url}}/g, url);
