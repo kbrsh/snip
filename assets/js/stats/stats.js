09daa6c0ffb72b52ad70be55f81df26ef7318449
Kabir Shah <simplecooldude1@gmail.com>
2016-09-14 20:38:37 -0700

    add get request util

--- assets/js/stats/stats.js
+++ assets/js/stats/stats.js
@@ -1,3 +1,14 @@
+// Util for Making GET request
+function httpGet(theUrl, callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous
+    xmlHttp.send(null);
+}
+
 if(document.getElementById("visits").innerHTML === "1") {
     document.getElementById("p").innerHTML = "person";
     document.getElementById("h").innerHTML = "has";
