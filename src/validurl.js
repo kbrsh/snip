08814c26cb80cb2e57516601ab87ac242abf152b
Gopal Shah <simplecooldude1@gmail.com>
2016-09-26 18:39:50 -0700

    expose validate

--- src/validurl.js
+++ src/validurl.js
@@ -1,5 +1,5 @@
 // RegEx for checking if URL is valid
-function ValidURL(str) {
+function validate(str) {
   var pattern = new RegExp(/([https|http]:\/\/)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]\/*)/);
   if(!pattern.test(str)) {
     return false;
@@ -7,3 +7,6 @@ function ValidURL(str) {
     return true;
   }
 }
+
+// Expose validate
+module.exports.validate = validate;
