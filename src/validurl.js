f44f477a8faa99faf53b3045e4473baaa9aa26f7
Gopal Shah <simplecooldude1@gmail.com>
2016-09-26 18:39:25 -0700

    valid url.js

---
+++ src/validurl.js
@@ -0,0 +1,9 @@
+// RegEx for checking if URL is valid
+function ValidURL(str) {
+  var pattern = new RegExp(/([https|http]:\/\/)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]\/*)/);
+  if(!pattern.test(str)) {
+    return false;
+  } else {
+    return true;
+  }
+}
