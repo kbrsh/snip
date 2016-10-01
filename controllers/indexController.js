dbcae792d60b1213622f44a1e747796be70dde5d
Gopal Shah <simplecooldude1@gmail.com>
2016-10-01 11:47:10 -0700

    dont use ejs for index

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,7 +1,6 @@
 var path = require("path");
-var ejs = require("ejs");
 
 module.exports = function(req, res) {
   // Send index.html file
-  res.render("index", {user: req.user})
+  res.sendFile(path.resolve(__dirname + "/../views/index.html"))
 }
