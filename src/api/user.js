6d78430f3067a943b76cb9124a18c4b46f29b6ac
Kabir Shah <simplecooldude1@gmail.com>
2016-10-01 15:25:44 -0700

    format user api

---
+++ src/api/user.js
@@ -0,0 +1,11 @@
+// formats the API into an object given blob of data
+var formatUserAPI = function(username, email, links, premium) {
+  return {
+    username: username,
+    email: email,
+    premium: premium,
+    links: links
+  }
+}
+
+module.exports = formatLinkAPI;
