0140fbb15f47bc66c7d4b602b6c29c5b1df5dc63
Gopal Shah <simplecooldude1@gmail.com>
2016-09-30 20:55:39 -0700

    use url controller

--- index.js
+++ index.js
@@ -38,26 +38,7 @@ app.get('/', indexController);
 
 
 // GET '/:id'
-app.get('/:id', function(req, res) {
-   //  Get ID from URL
-   var id = req.params.id;
-
-   //  Get URL from storage
-   storage.getURL(id).then(function(url) {
-      if(!url) {
-          // If it doesn't exist, send 404
-          util.showNotFound(res);
-      } else {
-          // If found
-          // Update stats
-          url.increment('visits', {by: 1});
-          // Redirect to URL
-          res.redirect(url.url);
-          //  Log it down
-          util.log("[SNIP] User redirected from /" + id, "green");
-      }
-   });
-});
+app.get('/:id', urlController);
 
 
 // Get '/:id/api'
