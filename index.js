5b45cc0a79e4397021ee3339e10ecee32239ceca
Kabir Shah <simplecooldude1@gmail.com>
2016-09-30 22:06:28 -0700

    getuser by id

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ var urlController = require("./controllers/urlController.js");
 
 // Setup auth
 passport.use(new Strategy(
-  function(username, password, cb) {
-    db.users.findByUsername(username, function(err, user) {
+  function(id, password, cb) {
+    storage.findUserById(id, function(err, user) {
       if (err) { return cb(err); }
       if (!user) { return cb(null, false); }
       if (user.password != password) { return cb(null, false); }
