f004c11a6aea5923758687319df8532df52a9ad1
Gopal Shah <simplecooldude1@gmail.com>
2016-09-30 22:11:58 -0700

    use promise

--- index.js
+++ index.js
@@ -31,12 +31,23 @@ var urlController = require("./controllers/urlController.js");
 // Setup auth
 passport.use(new Strategy(
   function(id, password, cb) {
-    storage.findUserById(id, function(user) {
+    storage.findUserById(id).then(function(user) {
       if (!user) { return cb(null, false); }
       if (user.password != password) { return cb(null, false); }
       return cb(null, user);
     });
   }));
+
+  passport.serializeUser(function(user, cb) {
+    cb(null, user.id);
+  });
+
+  passport.deserializeUser(function(id, cb) {
+    db.users.findById(id, function (err, user) {
+      if (err) { return cb(err); }
+      cb(null, user);
+    });
+  });
 
 
 // Express config
