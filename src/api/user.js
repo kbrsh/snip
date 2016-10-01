b5af410384ef97dc202f87e8c4a850bcc111fc81
Gopal Shah <simplecooldude1@gmail.com>
2016-10-01 15:38:13 -0700

    add avatar

--- src/api/user.js
+++ src/api/user.js
@@ -1,8 +1,9 @@
 // formats the API into an object given blob of data
-var formatUserAPI = function(username, email, links, premium) {
+var formatUserAPI = function(username, email, avatar, links, premium) {
   return {
     username: username,
     email: email,
+    avatar: avatar,
     premium: premium,
     links: links
   }
