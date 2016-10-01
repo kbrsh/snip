5a14c85c7155f50582992a7d5a5c8be2ff497a67
Kabir Shah <simplecooldude1@gmail.com>
2016-09-30 20:56:52 -0700

    urlController.api

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -1,5 +1,6 @@
 var storage = require("../models/storage.js");
-var util = require("../src/util.js")
+var util = require("../src/util.js");
+var api = require("../src/api/api.js")
 module.exports = function(req, res) {
    //  Get ID from URL
    var id = req.params.id;
@@ -17,6 +18,40 @@ module.exports = function(req, res) {
           res.redirect(url.url);
           //  Log it down
           util.log("[SNIP] User redirected from /" + id, "green");
+      }
+   });
+}
+
+// Get info of a URL
+module.exports.api = function(req, res) {
+   // get id from URL
+   var id = req.params.id;
+
+   // Set CORS, and content type to JSON
+   res.header('Content-Type', 'application/json');
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+
+   // Get URL from storage
+   storage.getURL(id).then(function(url) {
+      //  If doesn't exist, send ENOTFOUND in JSON
+      if(!url) {
+          res.send(JSON.stringify(api.formatAPIError("ENOTFOUND", "The requested URL is not valid, or is not found")));
+          // Log it down
+          util.log("[SNIP] ERR: Sending ENOTFOUND error", "yellow");
+      } else {
+          // Found
+          // Check to make pretty or not
+          if(req.query.pretty === "true") {
+            // Send formatted API Data
+            res.send(JSON.stringify(api.formatAPILink(url.id, url.visits, req.protocol + "://" + req.hostname + "/" + url.id, url.url), null, 3));
+          } else {
+            // Send nonformatted API Data
+            res.send(JSON.stringify(api.formatAPILink(url.id, url.visits, req.protocol + "://" + req.hostname + "/" + url.id, url.url)));
+          }
+
+          // Log it down
+          util.log("[SNIP] Sending API stats for /" + id, "green");
       }
    });
 }
--- index.js
+++ index.js
@@ -42,38 +42,7 @@ app.get('/:id', urlController);
 
 
 // Get '/:id/api'
-app.get('/:id/api', function(req, res) {
-   // get id from URL
-   var id = req.params.id;
-
-   // Set CORS, and content type to JSON
-   res.header('Content-Type', 'application/json');
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-
-   // Get URL from storage
-   storage.getURL(id).then(function(url) {
-      //  If doesn't exist, send ENOTFOUND in JSON
-      if(!url) {
-          res.send(JSON.stringify(api.formatAPIError("ENOTFOUND", "The requested URL is not valid, or is not found")));
-          // Log it down
-          util.log("[SNIP] ERR: Sending ENOTFOUND error", "yellow");
-      } else {
-          // Found
-          // Check to make pretty or not
-          if(req.query.pretty === "true") {
-            // Send formatted API Data
-            res.send(JSON.stringify(api.formatAPILink(url.id, url.visits, req.protocol + "://" + req.hostname + "/" + url.id, url.url), null, 3));
-          } else {
-            // Send nonformatted API Data
-            res.send(JSON.stringify(api.formatAPILink(url.id, url.visits, req.protocol + "://" + req.hostname + "/" + url.id, url.url)));
-          }
-
-          // Log it down
-          util.log("[SNIP] Sending API stats for /" + id, "green");
-      }
-   });
-});
+app.get('/:id/api', urlController.api);
 
 
 // GET '/:id/stats' and send stats
