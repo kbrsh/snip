7a04122bf6c357746094ed46957e72d2840aaf5a
Gopal Shah <simplecooldude1@gmail.com>
2016-10-02 17:08:57 -0700

    don't use passport

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ app.post('/auth/login',  passport.authenticate('local', { failureRedirect: '/?er
 });
 
 // POST "/auth/signup"
-app.post('/auth/login',  passport.authenticate('local', { failureRedirect: '/?error=Whoops!spaceThatspaceusernamespaceisspacetaken!' }), function(req, res) {
+app.post('/auth/login', function(req, res) {
     res.redirect('/');
 });
 
