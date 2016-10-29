bd0c6e2a2f1bfc4ead69a02c66bb4f21581ecfd7
Kabir Shah <simplecooldude1@gmail.com>
2016-10-29 12:37:15 -0700

    no controllers

--- controllers/dashboardController.js
+++
@@ -1,6 +0,0 @@
-var storage = require("../models/storage.js");
-
-module.exports = function(req, res) {
-  var links = req.user.links.split("#");
-  res.render('dashboard', {user: req.user, links: links});
-}
