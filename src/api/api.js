47d518af744e8cf91e10e61350dd223b2a01814e
Kabir Shah <simplecooldude1@gmail.com>
2016-10-29 12:37:30 -0700

    no src

--- src/api/api.js
+++
@@ -1,3 +0,0 @@
-module.exports.formatAPIError = require("./error.js");
-module.exports.formatAPILink = require("./link.js");
-module.exports.formatAPIUser = require("./user.js");
--- src/api/error.js
+++
@@ -1,8 +0,0 @@
-// Function to send error from api
-var formatAPIError = function(code, message) {
-  return {
-    error: code + ": " + message
-  }
-}
-
-module.exports = formatAPIError;
