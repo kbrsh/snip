d23e56e11533123dc4fc6a5b8fe8670bd64e6520
Kabir Shah <simplecooldude1@gmail.com>
2016-10-29 17:05:11 -0700

    connect to snip

--- model/model.js
+++ model/model.js
@@ -1,4 +1,4 @@
 var mongoose = require('mongoose');
 
-var mongo_url = process.env.OPENSHIFT_MONGODB_DB_URL || "mongodb://localhost/plate"
-mongoose.connect('mongodb://localhost:27017/plate');
+var mongo_url = process.env.OPENSHIFT_MONGODB_DB_URL || "mongodb://localhost/snip"
+mongoose.connect('mongodb://localhost:27017/snip');
