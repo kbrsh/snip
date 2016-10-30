b4fb9e5cb55abbec8ed8cad422a0ca7885953f30
Gopal Shah <simplecooldude1@gmail.com>
2016-10-29 17:00:24 -0700

    mongo

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ app.get('/', (req, res) => {
 
 // Listen
 app.listen(process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000, process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1", (req, res) => {
-
+  log("======= ✂️Snip ✂️ =======")
 });
