5cc7f2d75ce02333c45e137796b9691e19b265e8
Kabir Shah <simplecooldude1@gmail.com>
2016-10-29 17:10:43 -0700

    get req + schema

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,11 @@
 // Scripts
+
+function getReq(url, callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", url, true); // true for asynchronous
+    xmlHttp.send(null);
+}
