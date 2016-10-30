2665f971b35039de0a5b0259a6fc48ec1faa801f
Kabir Shah <simplecooldude1@gmail.com>
2016-10-29 17:13:50 -0700

    url schema

--- model/model.js
+++ model/model.js
@@ -3,3 +3,12 @@ var Schema = mongoose.Schema;
 
 var mongo_url = process.env.OPENSHIFT_MONGODB_DB_URL || "mongodb://localhost/snip"
 mongoose.connect('mongodb://localhost:27017/snip');
+
+var url = new Schema({
+  id: { type: String, required: true, unique: true },
+  shortURL: String,
+  longURL: String,
+  stats: {
+    visits: Number
+  }
+});
