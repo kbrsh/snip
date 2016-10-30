a5ece09015374b1f126ccf128ce7379f71146383
Kabir Shah <simplecooldude1@gmail.com>
2016-10-29 18:50:03 -0700

    post req

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,3 +9,18 @@ function getReq(url, callback) {
     xmlHttp.open("GET", url, true); // true for asynchronous
     xmlHttp.send(null);
 }
+
+
+function postReq(url, param, callback) {
+  var http = new XMLHttpRequest();
+  var params = "url=" + param;
+  http.open("POST", url, true);
+  http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+  http.onreadystatechange = function() {//Call a function when the state changes.
+    if(http.readyState == 4 && http.status == 200) {
+        alert(http.responseText);
+    }
+  }
+  http.send(params);
+}
