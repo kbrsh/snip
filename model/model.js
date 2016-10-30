aab3b156f9db1a405d4c3374decbc3ad1f3c4844
Gopal Shah <simplecooldude1@gmail.com>
2016-10-29 17:10:43 -0700

    get req + schema

--- model/model.js
+++ model/model.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
 var mongo_url = process.env.OPENSHIFT_MONGODB_DB_URL || "mongodb://localhost/snip"
 mongoose.connect('mongodb://localhost:27017/snip');
