9ae25bf19aa884300f989434e716e6a3b914943c
Kabir Shah <simplecooldude1@gmail.com>
2016-11-19 19:02:30 -0800

    get total visits

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -19,6 +19,7 @@ function removeProtocol(url) {
 function fillData(data) {
   document.getElementById("report-longURL").innerHTML = extractBaseURL(data.longURL);
   document.getElementById("report-shortURL").innerHTML = removeProtocol(data.shortURL);
+  document.getElementById("report-total-visits").innerHTML
 }
 
 var urlID = window.location.href.split("/")[3];
