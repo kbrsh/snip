164a820dd13ca79f79f9e8ab9ea758a13c246ab1
Kabir Shah <simplecooldude1@gmail.com>
2016-11-25 17:06:32 -0800

    make exports

--- src/apikey.js
+++ src/apikey.js
@@ -4,3 +4,8 @@ var key;
 crypto.randomBytes(16, function (err, bytes) {
     key = new Buffer((new Date().valueOf()) + (bytes).toString('hex')).toString('base64');
 });
+
+
+module.exports = (cb) => {
+  
+}
