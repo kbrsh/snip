f7534f408d20357d9905a08102c7516e6fe29403
Kabir Shah <simplecooldude1@gmail.com>
2016-11-25 17:07:21 -0800

    create key inside callback

--- src/apikey.js
+++ src/apikey.js
@@ -1,9 +1,8 @@
 var crypto = require('crypto');
-var key;
 
 module.exports = (cb) => {
   crypto.randomBytes(16, function (err, bytes) {
-      key = new Buffer((new Date().valueOf()) + (bytes).toString('hex')).toString('base64');
+      var key = new Buffer((new Date().valueOf()) + (bytes).toString('hex')).toString('base64');
       cb(key);
   });
 }
