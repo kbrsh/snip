9c129f3d6503b7e73112f86091aaad91b882af98
Kabir Shah <simplecooldude1@gmail.com>
2016-12-07 16:43:34 -0800

    make api key's smaller

--- src/apikey.js
+++ src/apikey.js
@@ -1,7 +1,7 @@
 var crypto = require('crypto');
 
 module.exports = (cb) => {
-  crypto.randomBytes(16, function (err, bytes) {
+  crypto.randomBytes(10, function (err, bytes) {
       var key = new Buffer((new Date().valueOf()) + (bytes).toString('hex')).toString('base64');
       cb(key);
   });
