1eca562ed7871c24481fabc12697ade9ed70a35f
Kabir Shah <simplecooldude1@gmail.com>
2016-12-08 21:26:28 -0800

    get request utility

---
+++ assets/js/random.js
@@ -0,0 +1,9 @@
+function getReq(url, callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", url, true);
+    xmlHttp.send(null);
+}
