a92562c8422ae36df07002721ae6f6edbabe1e52
Kabir Shah <simplecooldude1@gmail.com>
2016-12-08 18:10:58 -0800

    make api key

--- src/apikey.js
+++ src/apikey.js
@@ -1,5 +1,6 @@
 var crypto = require('crypto');
 
+// Generate API Key: To be used
 module.exports = (cb) => {
   crypto.randomBytes(10, function (err, bytes) {
       var key = new Buffer((new Date().valueOf()) + (bytes).toString('hex')).toString('base64');
